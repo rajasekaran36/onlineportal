e13695493a9de7d438e5b0d16e3af437acd705db
rajasekaran <proffraja@gmail.com>
2020-03-22 13:33:23 +0530

    test comment

--- src/main/java/com/kgisl/raja/onlineportal/OnlineportalApplication.java
+++ src/main/java/com/kgisl/raja/onlineportal/OnlineportalApplication.java
@@ -2,7 +2,7 @@ package com.kgisl.raja.onlineportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Main App
 @SpringBootApplication
 public class OnlineportalApplication {
 
